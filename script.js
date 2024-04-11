7a9646b20b2b2fe5ae4d68b127afd0f92cb71961
gouribhosale <67504431+gouribhosale@users.noreply.github.com>
2024-04-11 15:02:03 +0530

    Update script.js
    added new resume link

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
      $('#btndownload').click(function(e) {
      e.preventDefault();
-     window.location.href="gouribhosale_resume_updated.pdf";
+     window.location.href="gouri_resume_.pdf";
 
  });
 
