63862b37d8fad35885c992100effcf4555a2cf8e
gouribhosale <67504431+gouribhosale@users.noreply.github.com>
2024-07-27 22:12:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
      $('#btndownload').click(function(e) {
      e.preventDefault();
-     window.location.href="gourimain_resume.pdf";
+     window.location.href="gourimain_resume2.pdf";
 
  });
 
