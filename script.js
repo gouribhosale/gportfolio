5b9507d90b7de36cddd7d02cd8ad994752e39c98
gouribhosale <67504431+gouribhosale@users.noreply.github.com>
2025-01-20 14:45:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
      $('#btndownload').click(function(e) {
      e.preventDefault();
-     window.location.href="gouriresume_v2.0.pdf";
+     window.location.href="gouriresume_v2.1.pdf";
 
  });
 
